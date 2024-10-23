ab0fbfb55c66f96620afad769059e3e705e4c64c
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2024-10-23 18:39:05 -0400

    Se acomodo ComponentesConexas
    Se quito lo innecesario en ComponentesConexas, se agregaron los nombres como comentario y se ajustaron detalles en Adyacencias y Alcance

--- lab_sema_4/src/Main.kt
+++ lab_sema_4/src/Main.kt
@@ -25,6 +25,9 @@ fun getGrafo(rutaArchivo: String): Grafo? { // para leer el grafo de un archivo.
     return null
 }
 
+// Gabriel De Ornelas 15-10377
+// Mauricio Fragachan 20-10265
+
 fun getMatrizDeAdyacencia(g: Grafo): Matrix {
     var n = g.obtenerNumeroDeVertices()
     var A = Matrix(n,n) // el constructor por defecto inicializa en ceros
@@ -38,7 +41,7 @@ fun getMatrizDeAdyacencia(g: Grafo): Matrix {
                 //contiene a i (grafos no dirigidos) o que inicia en i (grafos dirigidos)
                 if( j == k.elOtroVertice(i) ){
                     //Al conseguirlo se asigna 1.0 en la casilla que representa al lado i,j
-                    A[i-1,j-1] = 1.0
+                    A.set(i-1, j-1, 1.0)
                 }
             }
         }
@@ -53,7 +56,7 @@ fun getMatrizDeAlcance(A: Matrix): Matrix {
     
     
     for (k in 0 until n) {
-        R[k,k] = 1.0    // Se agregan 1.0s en la diagonal principal (la matriz identidad) 
+        R.set(k, k, 1.0)    // Se agregan 1.0s en la diagonal principal (la matriz identidad) 
         for (i in 0 until n) {
             for (j in 0 until n) {
                 // Si no hay camino directo de i a j, pero hay un camino de i a k y de k a j, entonces hay un camino de i a j y
@@ -78,7 +81,7 @@ fun getComponentesConexas(g: Grafo): List<List<Int>> { //Construir una lista de 
     val visited = BooleanArray(n) // El constructor por defecto inicializa en False
     val components = mutableListOf<List<Int>>()
     
-    //Recorrer cada posible camino que conecta con el vertice i de la matriz de alcance
+    //Recorrer cada camino posible que conecte con el vertice i de la matriz de alcance
     for (i in 0 until n) {
         //Si el vertice no ha sido visitado, procedemos a buscar los vertices que estan conectados con el
         if (!visited[i]) {
@@ -86,49 +89,18 @@ fun getComponentesConexas(g: Grafo): List<List<Int>> { //Construir una lista de 
 
             //Agregamos el vertice a la lista de componentes, ya que en el peor caso que no este conectado con ningun otro vertice,
             //entonces el es una componente conexa por si solo
-            component.add(i)
+            component.add(i+1)  //Se agrega i+1 porque i es el indice en la matriz, i+1 es el vertice
 
-            //Recorrer los vertices que estan conectados con el vertice i
+            //Recorrer los vertices de la matriz de alcance buscando vertices que se alcancen mutuamente con i
             for (j in 0 until n) {
-
-                //Booleano para verificar si este vertice es parte de la componente conexa, es decir, si
-                //este vertice j esta conectado con todos los vertices de la componente
-                var parteDeComponente = true
                 //Primero, se verifica que i y j esten conectados
                 if (i != j && C.get(i, j) == 1.0 && C.get(j, i) == 1.0) {
-
-                    //Luego, se verifica que j este conectado con todos los vertices de la componente
-                    for(k in 0 until component.size){
-                        if(C.get(component[k],j) == 0.0 && C.get(j,component[k]) == 0.0){
-                            //Si no esta conectado con algun vertice de la componente, entonces no es parte de la componente, por lo
-                            //tanto se cambia el valor del booleano a false y se rompe el ciclo
-                            parteDeComponente = false
-                            break
-                        }
-                    }
-
-                    //Se verifica que el valor del booleano sea true para proceder a agregarlo a la componente conexa y
-                    //a marcarlo como visitado
-                    if(parteDeComponente){
-                        component.add(j)
-                        visited[j] = true
-                        C.set(j, i, 0.0)
-                        C.set(i, j, 0.0)  
-                    }
-
+                    component.add(j+1)  //Se agrega j+1 porque j es el indice en la matriz, j+1 es el vertice
+                    visited[j] = true  
                 }
             }
-            //Si la componente no esta vacia, entonces se agrega a la lista de componentes. Y ademas, antes de agregar la componente
-            //a dicha lista, se le debe sumar 1 a todos los vertices pertenecientes a ella, ya que los vertices dados en el grafo se empiezan
-            //a contar desde 1 y hasta ahora hemos trabajado es con los indices de las listas y por lo tanto, hemos estado contando desde 0 y no 
-            //desde 1 
-            if(component.isNotEmpty()){
-
-                for (k in 0 until component.size) {
-                    component[k] += 1
-                }
-                components.add(component)
-            }
+            //Se agrega la componente a la lista de componentes
+            components.add(component)
         }
     }
     
